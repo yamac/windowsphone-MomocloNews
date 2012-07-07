04a348cbefd9977bbcda1bf16d17f38c16939b34
yamac <dev@yamac.net>
2012-07-08 07:51:41 +0900

    add VersionAsFloat

--- MomocloNews/Helpers/AppAttributes.cs
+++ MomocloNews/Helpers/AppAttributes.cs
@@ -11,5 +11,19 @@ namespace Helpers
                 return PhoneHelper.GetAppAttribute("Version");
             }
         }
+        public static float VersionAsFloat
+        {
+            get
+            {
+                string version = PhoneHelper.GetAppAttribute("Version");
+                var versions = version.Split('.');
+                return
+                      int.Parse(versions[0])
+                    + (float)int.Parse(versions[1]) * 0.01f
+                    + (float)int.Parse(versions[2]) * 0.0001f
+                    + (float)int.Parse(versions[3]) * 0.000001f
+                    ;
+            }
+        }
     }
 }
